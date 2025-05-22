07a007a4114046937b46ac604e73dbddca20fe52
andifachreza2002@gmail.com <your.email@example.com>
2025-05-22 22:49:12 +0700

    install pillow untuk migrate gambar

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import PostListView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts/', PostListView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
