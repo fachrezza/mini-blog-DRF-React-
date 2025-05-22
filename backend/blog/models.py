da3e2a8bb2a562cf1dd264499e5c38ab6d446aa5
andifachreza2002@gmail.com <your.email@example.com>
2025-05-22 22:48:35 +0700

    menambahkan atribut gambar pada table post

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
